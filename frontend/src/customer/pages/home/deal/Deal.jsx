8dcd7d747cef48806ee55e9d0a172d46a58a1b4e
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-05-02 01:06:26 +0700

    update Deal

--- frontend/src/customer/pages/home/deal/Deal.jsx
+++ frontend/src/customer/pages/home/deal/Deal.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import DealCard from "./DealCard";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const Deal = () => {
-  return (
-    <div>Deal</div>
-  )
-}
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
 
-export default Deal
+    autoplay: true,
+    speed: 500, // 👈 animation nhanh thôi
+    autoplaySpeed: 2000, // 👈 delay giữa các lần chạy
+
+    cssEase: "ease-in-out",
+  };
+  return (
+    <div className="py-5 lg:px-20">
+      <div className="mb-5">
+        <Slider {...settings}>
+          {[1, 1, 1, 1, 1, 1].map((item, index) => (
+            <div key={index} className="px-2">
+              <div className="transform transition duration-300 hover:scale-105 hover:shadow-xl rounded-2xl">
+                <DealCard />
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default Deal;
--- frontend/src/customer/pages/home/deal/DealCard.jsx
+++ frontend/src/customer/pages/home/deal/DealCard.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const DealCard = () => {
   return (
-    <div>DealCard</div>
-  )
-}
+    <div className="w-[13rem] cursor-pointer">
+      <img src="https://images.unsplash.com/photo-1549298916-b41d501d3772" alt="" />
 
-export default DealCard
+      <div className="border-4 border-black bg-black text-white p-2 text-center">
+        <p className="text-lg font-semibold">Giày Sneaker</p>
+        <p className="text-2xl font-bold">Giảm 20%</p>
+        <p className="text-balance text-lg">Mua ngay</p>
+      </div>
+    </div>
+  );
+};
+
+export default DealCard;

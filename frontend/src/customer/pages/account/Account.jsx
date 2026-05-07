d2652d89d62bde8b96ac9777b29fc89b1ee7bda9
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-05-08 02:17:26 +0700

    update function Profile Account Order

--- frontend/src/customer/pages/account/Account.jsx
+++ frontend/src/customer/pages/account/Account.jsx
@@ -20,46 +20,48 @@ const Account = () => {
   };
 
   return (
-    <div className="px-4 lg:px-40 min-h-screen mt-10">
-      {/* HEADER */}
-      <div className="mb-5">
-        <h1 className="text-2xl font-bold text-gray-800">
-          Nguyễn Nam Trung Nguyên
-        </h1>
-        <p className="text-gray-500 text-sm">Quản lý tài khoản</p>
-      </div>
+    <div className="px-4 lg:px-20 min-h-screen mt-10 ">
+      <div className="bg-gray-50 p-5 rounded-xl shadow-sm">
+        {/* HEADER */}
+        <div className="mb-5">
+          <h1 className="text-2xl font-bold text-gray-800">
+            Nguyễn Nam Trung Nguyên
+          </h1>
+          <p className="text-gray-500 text-sm">Quản lý tài khoản</p>
+        </div>
 
-      <Divider />
+        <Divider />
 
-      {/* Left */}
-      <div className="grid grid-cols-1 lg:grid-cols-4 gap-5 mt-5">
-        {/* SIDEBAR */}
-        <div className="bg-white shadow-md rounded-2xl p-3 flex lg:flex-col flex-row flex-wrap gap-2 h-fit">
-          {menu.map((item, index) => {
-            const isActive = location.pathname.includes(item.path);
+        {/* Left */}
+        <div className="grid grid-cols-1 lg:grid-cols-4 gap-5 mt-5">
+          {/* SIDEBAR */}
+          <div className="bg-white shadow-md rounded-2xl p-3 flex lg:flex-col flex-row flex-wrap gap-2 h-fit">
+            {menu.map((item, index) => {
+              const isActive = location.pathname.includes(item.path);
 
-            return (
-              <div
-                onClick={() => handleClick(item)}
-                key={index}
-                className={`flex items-center justify-between px-4 py-3 rounded-xl cursor-pointer transition-all duration-200
+              return (
+                <div
+                  onClick={() => handleClick(item)}
+                  key={index}
+                  className={`flex items-center justify-between px-4 py-3 rounded-xl cursor-pointer transition-all duration-200
                   ${
                     isActive
                       ? "bg-gray-800 text-white shadow"
                       : "hover:bg-gray-100 text-gray-700"
                   }
                 `}
-              >
-                <span className="font-medium">{item.name}</span>
+                >
+                  <span className="font-medium">{item.name}</span>
 
-                {item.name === "Đăng xuất" && <IoLogOut className="white" />}
-              </div>
-            );
-          })}
+                  {item.name === "Đăng xuất" && <IoLogOut className="white" />}
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
 
-      {/* Right */}
+        {/* Right */}
+      </div>
     </div>
   );
 };

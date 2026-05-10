acc3ffa75244bf624c0958b2623d691c1eb520b9
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-05-10 19:40:36 +0700

    update eror

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useRoutes } from "react-router-dom";
 
-import { customerRoutes } from "./customerRoutes";
-import { sellerRoutes } from "./sellerRoutes";
+import { customerRoutes } from "./CustomerRoutes";
+import { sellerRoutes } from "./SellerRoutes";
 
 const AppRoutes = () => {
   const routing = useRoutes([...customerRoutes, ...sellerRoutes]);
